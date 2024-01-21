8685bbb4d6d86665790997be526d23d2f7623410
Rashika Varshney <rashika251298.rv@pumpumpum.com>
2024-01-21 23:54:55 +0530

    added backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiBaseUrl: 'http://localhost:3000/api',
+    production: true,
+    apiBaseUrl: 'https://portfolio-backend-gdwz.onrender.com/api',
 };
